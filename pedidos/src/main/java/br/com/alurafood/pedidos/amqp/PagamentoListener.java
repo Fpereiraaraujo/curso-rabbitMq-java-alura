d0d8aa45fb737f9e5e80e2df5155ce3dfe3f9e8c
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2025-03-03 22:13:42 -0300

    feat:personalizando o recebimento das mensagens com daods personalizados

--- pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -1,5 +1,6 @@
 package br.com.alurafood.pedidos.amqp;
 
+import br.com.alurafood.pedidos.dto.PagamentoDTO;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,8 +11,16 @@ import org.springframework.stereotype.Component;
 public class PagamentoListener {
 
     @RabbitListener(queues = "pagamento.concluido")
-public void recebeMensagem(Message message){
-    System.out.println("recebi a menssagem" + message);
-}
+    public void recebeMensagem(PagamentoDTO pagamento) {
+
+        String mensagem = """
+                        Dados do pagamento: %s
+                Número do pedido: %s
+                Valor R$: %s
+                Status: %s 
+                     """.formatted(pagamento.getId(), pagamento.getPedidoId(), pagamento.getValor(),pagamento.getStatus());
+        System.out.println("recebi a menssagem" + mensagem);
+    }
+
 
 }
